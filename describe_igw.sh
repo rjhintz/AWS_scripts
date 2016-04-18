867af20d98397df07d31ae9858c0f222659bfa0a
Rich Hintz <rjhintz@gmail.com>
2016-04-17 19:59:42 -0700

    add VPC filtering

--- describe_igw.sh
+++ describe_igw.sh
@@ -5,6 +5,30 @@
 export AWS_DEFAULT_REGION=us-east-1
 echo "AWS_DEFAULT_REGION= " $AWS_DEFAULT_REGION
 #
+#
+# Is there a VPC with CIDR 172.16.0.0/16?
+default_cidr="172.16.0.0/16"
+read -p "CIDR? (default: $default_cidr)  " cidr
+#
+# if $cidr value that's input has regex pattern ^$, it's empty, so use default
+if [[ "$cidr" =~ ^$ ]]; then
+    cidr=$default_cidr
+    echo "using default CIDR $default_cidr";
+fi
+#
+# Describe VPCs
+# Describe VPC results fields
+# 1- "VPCS"
+# 2- CidrBlock
+# 3- DhcpOptionsId
+# 4- InstanceTenancy
+# 5- IsDefault
+# 6- State
+# 7- VpcId
+#
+# get VpcId from field 7
+vpc=$(aws ec2 describe-vpcs --output text --filters Name=cidr,Values=$cidr | cut -f 7)
+#
 # Describe IGWs
 # Describe IGW results fields
 # 1- "INTERNETGATEWAYS"
@@ -12,4 +36,9 @@ echo "AWS_DEFAULT_REGION= " $AWS_DEFAULT_REGION
 # 3- Attachments: State
 # 4- Attachments: VpcId
 #
-aws ec2 describe-internet-gateways --output text #  --filters Name=attachment.vpc-id,Values=$vpc
+aws ec2 describe-internet-gateways --output text \
+    --filters Name=attachment.vpc-id,Values=$vpc \
+    > describe_igw.txt
+igw=$(egrep --only-matching igw-[a-z0-9]{8} describe_igw.txt)
+echo "IGW for" $cidr "vpc" $vpc "is" $igw
+
