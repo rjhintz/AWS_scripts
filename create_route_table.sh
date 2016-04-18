aa95792d052000108875f4221351de1e651416f9
Rich Hintz <rjhintz@gmail.com>
2016-04-18 09:44:08 -0700

    Add logic to create route & associate RTB with subnet
    create route to public internet
    associate RTB with new route to one subnet
    need to do the same for a subnet in the other AZ

--- create_route_table.sh
+++ create_route_table.sh
@@ -64,19 +64,41 @@ else
 fi
 #
 # Describe Subnets, then get those for public
-
 aws ec2 describe-subnets --output text  --filters Name=vpc-id,Values=$vpc \
     > describe_subnets.txt
+#
+# Describe IGW
+aws ec2 describe-internet-gateways --output text \
+    --filters Name=attachment.vpc-id,Values=$vpc \
+    > describe_igw.txt
+igw=$(egrep --only-matching igw-[a-z0-9]{8} describe_igw.txt)
+#
 # Create Route Table
 aws ec2 create-route-table --vpc-id $vpc > create_route_table.json
-rtb_id=$(egrep rtb-[A-Za-z0-9]{8} create_route_table.json)
+rtb_id=$(egrep --only-matching rtb-[a-z0-9]{8} create_route_table.json)
 echo " "
-echo "rtb created = " $rtb_id
+echo "rtb created" $rtb_id "for VPC" $vpc
 #
 # Create Route
-# aws ec2 create-route --route-table-id $rtb_id \
-#     --destination-cidr-block xxx
+cidr_all=0.0.0.0/0
+aws ec2 create-route --route-table-id $rtb_id \
+    --destination-cidr-block $cidr_all  \
+    --gateway-id $igw
+echo "Route created for" $cidr_all "to IGW" $igw
 #
 # Associate RTB(s) with Subnet(s)
+aws ec2 describe-subnets --output text \
+    --filters Name=vpc-id,Values=$vpc  \
+    > describe_subnets.txt
+subnet_public_1=$(egrep 172.16.1.0 describe_subnets.txt  \
+    | egrep --only-matching subnet-[a-z0-9]{8})
+echo "subnet_public_1" $subnet_public_1
+echo ""
 #
-
+subnet_public_2=$(egrep 172.16.11.0 describe_subnets.txt  \
+    | egrep --only-matching subnet-[a-z0-9]{8})
+echo "subnet_public_2" $subnet_public_2
+aws ec2 associate-route-table \
+    --subnet-id subnet_public_1  \
+    --route-table-id $rtb_id
+#echo "Route table" $rtb-id "subnet" $subnet-id
