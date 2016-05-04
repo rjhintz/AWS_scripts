ce16170f7c93bbe47cf762665bfd919f96ae1941
Rich Hintz <rjhintz@gmail.com>
2016-05-04 16:07:46 -0700

    fix array handling

--- describe_availability_zones.sh
+++ describe_availability_zones.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Describe AWS AZs
 #
@@ -7,16 +7,20 @@ echo "AWS_DEFAULT_REGION= " $AWS_DEFAULT_REGION
 #
 # Put the AZs into an array
 az=($(aws ec2 describe-availability-zones --output text | cut -f 4))
+# curly braces are required when expanding arrays
+# http://stackoverflow.com/questions/8748831/when-do-we-need-curly-braces-in-variables-using-bash
+# echo ${az[*]}
 #
-index=0
+echo "AZs "
 # How many are there?
 az_count=${#az[*]}
-# echo "AZ count " $az_count
+echo "AZ count" $az_count
 #
 # List AZs
+index=0
 while [ $index -lt $az_count ]
 do
-#   echo ${az[$index]}
-    # arithmetic evaluation syntax ((x))
+    echo ${az[$index]}
+    # arithmetic evaluation syntax: ((x))
     ((index++))
 done;
