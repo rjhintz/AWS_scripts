039a1e35aea87956e2fb57967352fc9a030b161d
Rich Hintz <rjhintz@gmail.com>
2016-05-06 12:31:50 -0700

    initial - associate route table only

---
+++ associate_route_table.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+#
+# AWS - Create Route Table, then Create Route, and then Associate Route Table
+#  with Subnet
+#
+export AWS_DEFAULT_REGION=us-east-1
+echo "AWS_DEFAULT_REGION= " $AWS_DEFAULT_REGION
+#
+# Describe Current Route Tables
+# Describe RT results fields
+#"RouteTables":
+#    "Associations":
+#    "RouteTableAssociationId": "rtbassoc-854d6be2",
+#        "Main": true,
+#        "RouteTableId": "rtb-ec7abd8b"
+#    "Tags": [],
+#    "Routes": [
+#        "Origin": "CreateRouteTable",
+#        "State": "active",
+#        "DestinationCidrBlock": "172.30.0.0/16",
+#        "GatewayId": "local"
+#
+#        "Origin": "CreateRoute",
+#        "State": "active",
+#        "DestinationCidrBlock": "0.0.0.0/0",
+#        "GatewayId": "igw-0fa1166b"
+aws ec2 describe-route-tables --output json > describe_route_tables.json
+#
+# Get VPC value
+#
+# Is there a VPC with CIDR 172.16.0.0/16?
+default_cidr="172.16.0.0/16"
+read -p "CIDR? (default: $default_cidr)  " cidr
+#
+# if $cidr value that's input has regex pattern ^$, it's empty, so use default
+if [[ "$cidr" =~ ^$ ]]; then
+    cidr=$default_cidr
+    echo "using default CIDR $default_cidr";
+fi
+#
+# Describe VPCs
+# Describe VPC results fields
+# 1- "VPCS"
+# 2- CidrBlock
+# 3- DhcpOptionsId
+# 4- InstanceTenancy
+# 5- IsDefault
+# 6- State
+# 7- VpcId
+#
+# get VpcId from field 7
+vpc=$(aws ec2 describe-vpcs --output text --filters Name=cidr,Values=$cidr | cut -f 7)
+# if $vpc = regex ^$, then no VPC with specified CIDR
+if [[ "$vpc" =~ ^$ ]]; then \
+    echo "No VPC with CIDR $cidr"
+else
+    aws ec2 describe-vpcs --output json --filters Name=cidr,Values=$cidr \
+        > describe_vpcs.json
+fi
+#
+# Describe Subnets in VPC, then get those for public
+aws ec2 describe-subnets --output text  --filters Name=vpc-id,Values=$vpc \
+    > describe_subnets.txt
+#
+# Get IGW value for VPC
+aws ec2 describe-internet-gateways --output text \
+    --filters Name=attachment.vpc-id,Values=$vpc \
+    > describe_igw.txt
+igw=$(egrep --only-matching igw-[a-z0-9]{8} describe_igw.txt)
+#
+# Associate RTB(s) with Subnet(s)
+aws ec2 describe-subnets --output text \
+    --filters Name=vpc-id,Values=$vpc  \
+    > describe_subnets.txt
+subnet_public_1=$(egrep 172.16.1.0 describe_subnets.txt  \
+    | egrep --only-matching subnet-[a-z0-9]{8})
+echo " "
+echo "subnet_public_1" $subnet_public_1
+#
+subnet_public_2=$(egrep 172.16.11.0 describe_subnets.txt  \
+    | egrep --only-matching subnet-[a-z0-9]{8})
+echo ""
+echo "subnet_public_2" $subnet_public_2
+#
+# Associate route table for first public subnet
+aws ec2 associate-route-table \
+    --subnet-id $subnet_public_1  \
+    --route-table-id $rtb_id
+#echo "Route table" $rtb-id "subnet" $subnet-id
